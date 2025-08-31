249c34547f569649cb86e0f929f9ad8d0589d5c5
Anirudh Patwal <161865581+CapThunder19@users.noreply.github.com>
2025-08-31 22:53:18 +0530

    Change API proxy URL to production endpoint

--- frontend/streetfood/vite.config.js
+++ frontend/streetfood/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [tailwindcss(),react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3001',
+      '/api': 'https://chatori-website.onrender.com/',
     },
   },
 })
